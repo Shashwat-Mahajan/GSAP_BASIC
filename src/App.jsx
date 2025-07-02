57ea74615c45d4194caff3e805322f4e3ab12dff
Shashwat Mahajan <shashwatmahajan2@gmail.com>
2025-07-03 00:33:14 +0530

    Ochi Design Start landing Page Built

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,62 @@
-import React, { use } from "react";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { useRef } from "react";
-import { useState } from "react";
+// import React, { use } from "react";
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+// import { useRef } from "react";
+// import { useState } from "react";
+
+// const App = () => {
+//   const [circle, setCircle] = useState(0);
+//   const [circley, setCircley] = useState(0);
+//   const [rotateq, setRotateq] = useState(0);
+//   const random = gsap.utils.random(-500, 500, 100);
+//   const randomy = gsap.utils.random(-500, 500, 100);
+//   const rotate = gsap.utils.random(-200, 200, 30);
+
+//   const imageRef = useRef();
+
+//   const { contextSafe } = useGSAP();
+
+//   const rotateBox = contextSafe(() => {
+//     gsap.to(imageRef.current, {
+//       duration: 0.5,
+//       x: circle,
+//       rotate: rotateq,
+//       y: circley,
+//     });
+//   });
+
+//   return (
+//     <main>
+//       <button
+//         onClick={() => {
+//           setCircle(random);
+//           setRotateq(rotate);
+//           setCircley(randomy);
+//           console.log(random);
+//           rotateBox();
+//         }}
+//       >
+//         MOve
+//       </button>
+//       <img ref={imageRef} src="../images/messi.png"></img>
+//     </main>
+//   );
+// };
+
+// export default App;
+
+
+import React from 'react'
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
 
 const App = () => {
-  const [circle, setCircle] = useState(0);
-  const [circley, setCircley] = useState(0);
-  const [rotateq, setRotateq] = useState(0);
-  const random = gsap.utils.random(-500, 500, 100);
-  const randomy = gsap.utils.random(-500, 500, 100);
-  const rotate = gsap.utils.random(-200, 200, 30);
-
-  const imageRef = useRef();
-
-  const { contextSafe } = useGSAP();
-
-  const rotateBox = contextSafe(() => {
-    gsap.to(imageRef.current, {
-      duration: 0.5,
-      x: circle,
-      rotate: rotateq,
-      y: circley,
-    });
-  });
-
   return (
-    <main>
-      <button
-        onClick={() => {
-          setCircle(random);
-          setRotateq(rotate);
-          setCircley(randomy);
-          console.log(random);
-          rotateBox();
-        }}
-      >
-        MOve
-      </button>
-      <img ref={imageRef} src="../images/messi.png"></img>
-    </main>
-  );
-};
+    <div className='w-full h-screen bg-zinc-900'>
+      <Navbar />
+      <Landing />
+    </div>
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/Landing.jsx
@@ -0,0 +1,50 @@
+import {MousePointer2 } from "lucide-react";
+import React from "react"; // Assuming you have a CSS file for styles
+const Landing = () => {
+  return (
+    <div>
+      <div className="w-full min-h-screen bg-zinc-900 pt-2">
+        <div className="textstructure mt-40 px-20 md:px-16 lg:px-20">
+          {["We Create", "Eye Opening", "Presentations"].map((item, index) => {
+            return (
+              <div className="masker flex sm:mb-6">
+                {index === 1 && (
+                  <div className="mr-5 sm:mr-5 rounded-md w-[10vw] sm:w-[10vw] h-[4.5vw] sm:h-[4.5vw] relative top-[0.8vw] bg-white"></div>
+                )}
+                <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-[Orbitron] leading-[6vw] sm:leading-[6vw] tracking-tighter flex text-white font-semibold relative">
+                  {item}
+                </h1>
+              </div>
+            );
+          })}
+
+          <div className="border-t-[1px] border-zinc-600 mt-32 sm:mt-32 flex flex-col sm:flex-row justify-between items-start sm:items-center py-5 sm:gap-0">
+            {[
+              "For Public And Privaet Companies",
+              "From The First Pitch To IPO",
+            ].map((item, index) => {
+              return (
+                <p
+                  key={index}
+                  className="text-sm sm:text-mdfont-light tracking-tight leading-none text-white"
+                >
+                  {item}
+                </p>
+              );
+            })}
+            <div className="start flex items-center gap-5">
+              <div className="px-5 sm:px-5 border-[2px] border-zinc-500 text-white sm:text-base font-light rounded-full py-2">
+                Start The Project
+              </div>
+              <div className="w-10 h-10 flex items-center justify-center text-white border-[2px] border-zinc-400 rounded-full">
+                <MousePointer2 className="rotate-90 w-5 h-5" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
